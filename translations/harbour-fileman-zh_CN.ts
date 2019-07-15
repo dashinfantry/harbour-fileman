2da08928d1e4ccb3ffab03f6b851592bb14a71d2
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-07-15 17:47:11 +0800

    Update harbour-fileman-zh_CN.ts

--- translations/harbour-fileman-zh_CN.ts
+++ translations/harbour-fileman-zh_CN.ts
@@ -88,11 +88,11 @@
     </message>
     <message>
         <source>1 entry in clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>1 个项目已保存到剪贴板</translation>
     </message>
     <message>
         <source> entries in clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation> 个项目已保存到剪贴板</translation>
     </message>
 </context>
 <context>
@@ -146,7 +146,7 @@
     </message>
     <message>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>搜索</translation>
     </message>
 </context>
 <context>
@@ -338,11 +338,11 @@
     </message>
     <message>
         <source>ZIP archive</source>
-        <translation type="unfinished"></translation>
+        <translation>ZIP 文档</translation>
     </message>
     <message>
         <source>TAR.GZ archive</source>
-        <translation type="unfinished"></translation>
+        <translation ></translation>
     </message>
 </context>
 <context>
@@ -523,11 +523,11 @@
     <name>SearchPage</name>
     <message>
         <source>Fileman Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Fileman 搜索</translation>
     </message>
     <message>
         <source>The search option is still a work in progress, and do not function.</source>
-        <translation type="unfinished"></translation>
+        <translation>搜索功能尚处于开发阶段，暂时无法工作。</translation>
     </message>
 </context>
 <context>
