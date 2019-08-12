d2d6e23a65754cdd7bf5e8f205d9af920f07f85e
Rudi Timmermans <rudi.timmer@mail.ch>
2019-08-12 10:45:57 +0200

    Adding more support for documents and excel

--- src/globals.cpp
+++ src/globals.cpp
@@ -5,17 +5,15 @@
 QString suffixToIconName(QString suffix)
 {
     // only formats that are understood by File Browser or Sailfish get a special icon
-    if (suffix == "txt")
+    if (suffix == "txt" || suffix == "doc" || suffix == "docx" || suffix == "xls" || suffix == "xlsx" || suffix == "pdf")
         return "file-txt";
     if (suffix == "rpm")
         return "file-rpm";
     if (suffix == "apk")
         return "file-apk";
-    if (suffix == "png" || suffix == "jpeg" || suffix == "jpg" ||
-            suffix == "gif" || suffix == "svg")
+    if (suffix == "png" || suffix == "jpeg" || suffix == "jpg" || suffix == "gif" || suffix == "svg")
         return "file-image";
-    if (suffix == "wav" || suffix == "mp3" || suffix == "flac" ||
-            suffix == "aac" || suffix == "ogg" || suffix == "m4a")
+    if (suffix == "wav" || suffix == "mp3" || suffix == "flac" || suffix == "aac" || suffix == "ogg" || suffix == "m4a")
         return "file-audio";
     if (suffix == "mp4" || suffix == "m4v")
         return "file-video";
